f729f08b8089b6a9b2d8ef6d2aaa90715192836b
Danylo Storozhev <daniel@storozhev.net>
2025-01-13 11:08:30 +0000

    fix duplicate config file in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -167,32 +167,6 @@ EXTRA_IMAGE_FEATURES = "debug-tweaks"
 EOL
 fi
 
-echo "Creating local.conf..."
-cat > /yocto/build/conf/local.conf << EOL
-MACHINE ??= '${MACHINE}'
-DISTRO ?= '${DISTRO}'
-MENDER_ARTIFACT_NAME = "release-1"
-INHERIT += "mender-full"
-ARTIFACTIMG_FSTYPE = "ext4"
-INIT_MANAGER = "systemd"
-DISTRO_VERSION = "0.0.1"
-OLDEST_KERNEL = "5.4.24"
-PREFERRED_PROVIDER_u-boot = "u-boot-imx"
-PREFERRED_PROVIDER_virtual/bootloader = "u-boot-imx"
-PREFERRED_PROVIDER_virtual/kernel="linux-imx"
-PREFERRED_VERSION_linux_imx = "5.4.24"
-PREFERRED_VERSION_u-boot-imx = "2017.03"
-EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
-USER_CLASSES ?= "buildstats"
-PATCHRESOLVE = "noop"
-PACKAGECONFIG:append:pn-qemu-system-native = " sdl"
-CONF_VERSION = "2"
-DL_DIR ?= "${BSPDIR}/downloads/"
-ACCEPT_FSL_EULA = "1"
-HOSTTOOLS += "x86_64-linux-gnu-gcc git-lfs python"
-EXTRA_IMAGE_FEATURES = "debug-tweaks"
-EOL
-
 echo "Starting build process..."
 
 bitbake "librescoot-${TARGET}-image" --continue
